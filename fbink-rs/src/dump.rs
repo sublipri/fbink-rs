3a74ec508af50c37b16761b25a2274f10314a2a5
sublipri <sublipri@proton.me>
2024-02-23 13:24:24 +0800

    Rework dump and restore and add region_dump
    I'd originally assumed that FBInk 'recycling' the FBInkDump struct meant
    it doesn't allocate a new buffer, but looking at fbink.c it appears to
    free the existing data and then re-allocate. So I don't think(?) there's
    any major benefit to re-using the raw::FBInkDump struct like before.
    This commit reworks things into what should be a nicer interface to
    fbink_dump and fbink_restore

---
+++ fbink-rs/src/dump.rs
@@ -0,0 +1,168 @@
+use crate::error::FbInkError;
+
+use std::fs;
+use std::io::Cursor;
+use std::path::PathBuf;
+use std::process::Command;
+use std::slice;
+
+use fbink_sys as raw;
+use image::{imageops, ColorType, DynamicImage, ImageOutputFormat};
+use proc_mounts::MountIter;
+
+#[derive(Debug, Clone)]
+pub struct FbInkDump {
+    raw: raw::FBInkDump,
+}
+
+impl Drop for FbInkDump {
+    fn drop(&mut self) {
+        unsafe { raw::fbink_free_dump_data(&mut self.raw) };
+    }
+}
+
+impl FbInkDump {
+    pub fn new(raw: raw::FBInkDump) -> Self {
+        Self { raw }
+    }
+    pub fn data(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts_mut(self.raw.data, self.raw.size) }
+    }
+    pub fn size(&self) -> usize {
+        self.raw.size
+    }
+    pub fn stride(&self) -> usize {
+        self.raw.stride
+    }
+    pub fn area(&self) -> raw::FBInkRect {
+        self.raw.area
+    }
+    pub fn clip(&self) -> raw::FBInkRect {
+        self.raw.clip
+    }
+    pub fn rota(&self) -> u8 {
+        self.raw.rota
+    }
+    pub fn bpp(&self) -> u8 {
+        self.raw.bpp
+    }
+    pub fn is_full(&self) -> bool {
+        self.raw.is_full
+    }
+    pub fn raw_dump(&self) -> &raw::FBInkDump {
+        &self.raw
+    }
+    /// Crop the regions of the dump. Doesn't touch the actual data but affects calls to restore
+    pub fn crop(&mut self, left: u16, top: u16, width: u16, height: u16) {
+        self.raw.clip.left = left;
+        self.raw.clip.top = top;
+        self.raw.clip.width = width;
+        self.raw.clip.height = height;
+        self.raw.is_full = false;
+    }
+    /// Encode the dump's data in the given image format and return the bytes
+    pub fn encode(&self, encoding: ImageOutputFormat) -> Result<Vec<u8>, FbInkError> {
+        let color_type = match self.bpp() {
+            32 => ColorType::Rgba8,
+            24 => ColorType::Rgb8,
+            // not sure if these are correct
+            16 => ColorType::L16,
+            8 => ColorType::L8,
+            _ => {
+                let msg = format!("Can't encode a dump with {} bpp", self.bpp());
+                return Err(FbInkError::NotSupported(msg));
+            }
+        };
+        let mut writer = Cursor::new(Vec::new());
+        image::write_buffer_with_format(
+            &mut writer,
+            self.data(),
+            self.area().width.into(),
+            self.area().height.into(),
+            color_type,
+            encoding,
+        )
+        .unwrap();
+        Ok(writer.into_inner())
+    }
+    /// Clone the dump's data and convert it to a DynamicImage
+    pub fn dynamic_image(&self) -> Result<DynamicImage, FbInkError> {
+        type Gray16Image = image::ImageBuffer<image::Luma<u16>, Vec<u16>>;
+        let width = self.area().width as u32;
+        let height = self.area().height as u32;
+        let err_msg = "Unable to convert dump to DynamicImage";
+        let image = match self.bpp() {
+            32 => {
+                let buf = self.data().to_vec();
+                DynamicImage::ImageRgba8(image::RgbaImage::from_raw(width, height, buf).unwrap())
+            }
+            24 => {
+                let buf = self.data().to_vec();
+                DynamicImage::ImageRgb8(image::RgbImage::from_raw(width, height, buf).unwrap())
+            }
+            8 => {
+                let buf = self.data().to_vec();
+                DynamicImage::ImageLuma8(image::GrayImage::from_raw(width, height, buf).unwrap())
+            }
+            // Not sure if this is the correct thing to do.
+            16 => {
+                let (prefix, shorts, suffix) = unsafe { self.data().align_to::<u16>() };
+                if !prefix.is_empty() || !suffix.is_empty() {
+                    return Err(FbInkError::NotSupported(err_msg.into()));
+                }
+                let buf = shorts.to_vec();
+                DynamicImage::ImageLuma16(Gray16Image::from_raw(width, height, buf).unwrap())
+            }
+            _ => return Err(FbInkError::NotSupported(err_msg.into())),
+        };
+        Ok(image)
+    }
+}
+
+// Adapted from FBInk/utils/sunxigrab.sh
+pub fn dump_sunxi(
+    encoding: Option<ImageOutputFormat>,
+    current_rota: u8,
+) -> Result<Vec<u8>, FbInkError> {
+    // Path is hard-coded by the kernel
+    let mount_path = PathBuf::from("/mnt/flash");
+    let bmp_path = mount_path.join("workingbuffer.bmp");
+    fs::create_dir_all(&mount_path).unwrap();
+
+    if !MountIter::new()?.any(|m| m.unwrap().dest == mount_path) {
+        Command::new("mount")
+            .arg("-t")
+            .arg("tmpfs")
+            .arg("tmpfs")
+            .arg(mount_path)
+            .arg("-o")
+            .arg("noatime,size=3M")
+            .output()?;
+    }
+
+    // Trigger a dump of the framebuffer by reading from the sysfs file
+    let sysfs_path = "/sys/devices/virtual/disp/disp/waveform/get_working_buffer";
+    let rv: i32 = fs::read_to_string(sysfs_path)?.trim().parse().unwrap();
+
+    if rv == 0 {
+        println!("Working buffer dumped to {}", bmp_path.display());
+    } else {
+        println!("Failed to dump the working buffer!");
+    }
+    let mut decoded = image::io::Reader::open(bmp_path)?.decode()?;
+    imageops::flip_vertical_in_place(&mut decoded);
+    let decoded = match current_rota {
+        0 => decoded.rotate270(),
+        2 => decoded.rotate90(),
+        3 => decoded.rotate180(),
+        _ => decoded,
+    };
+
+    if let Some(encoding) = encoding {
+        let mut encoded = Cursor::new(Vec::new());
+        decoded.write_to(&mut encoded, encoding)?;
+        Ok(encoded.into_inner())
+    } else {
+        Ok(decoded.into_bytes())
+    }
+}
--- fbink-rs/src/lib.rs
+++ fbink-rs/src/lib.rs
@@ -3,19 +3,14 @@ use crate::error::FbInkError;
 pub use crate::state::{CanonicalRotation, FbInkState};
 
 use std::ffi::CString;
-use std::fs;
-use std::io::Cursor;
 use std::mem::MaybeUninit;
-use std::path::PathBuf;
-use std::process::Command;
-use std::slice;
 
+use dump::{dump_sunxi, FbInkDump};
 use fbink_sys as raw;
 pub use image::ImageOutputFormat;
-use image::{imageops, ColorType};
-use proc_mounts::MountIter;
 
 pub mod config;
+pub mod dump;
 pub mod error;
 pub mod state;
 
@@ -23,15 +18,11 @@ pub mod state;
 pub struct FbInk {
     pub config: FbInkConfig,
     fbfd: std::os::raw::c_int,
-    raw_dump: Option<raw::FBInkDump>,
 }
 
 impl Drop for FbInk {
     fn drop(&mut self) {
         unsafe { raw::fbink_close(self.fbfd) };
-        if let Some(ref mut raw_dump) = self.raw_dump {
-            unsafe { raw::fbink_free_dump_data(raw_dump) };
-        }
     }
 }
 
@@ -48,11 +39,7 @@ impl FbInk {
         // returns negative error codes
         let rv = unsafe { raw::fbink_init(fbfd, &config.into()) };
         match -rv {
-            libc::EXIT_SUCCESS => Ok(Self {
-                config,
-                fbfd,
-                raw_dump: None,
-            }),
+            libc::EXIT_SUCCESS => Ok(Self { config, fbfd }),
             libc::EXIT_FAILURE => Err(FbInkError::ExitFailure("init".into())),
             libc::ENOSYS => Err(FbInkError::NotSupported(
                 "Your device is not supported by FBInk".into(),
@@ -119,15 +106,12 @@ impl FbInk {
         self.print(msg)
     }
 
-    /// # Safety
-    ///
-    /// See fbink_dump in fbink.h
-    pub unsafe fn raw_dump(&mut self) -> Result<raw::FBInkDump, FbInkError> {
+    /// Dump the contents of the framebuffer
+    pub fn dump(&self) -> Result<FbInkDump, FbInkError> {
         let mut dump = MaybeUninit::<raw::FBInkDump>::zeroed();
-        // returns negative error codes
-        let rv = raw::fbink_dump(self.fbfd, dump.as_mut_ptr());
+        let rv = unsafe { raw::fbink_dump(self.fbfd, dump.as_mut_ptr()) };
         match -rv {
-            libc::EXIT_SUCCESS => Ok(dump.assume_init()),
+            libc::EXIT_SUCCESS => Ok(FbInkDump::new(unsafe { dump.assume_init() })),
             libc::EXIT_FAILURE => Err(FbInkError::ExitFailure("dump".into())),
             libc::ENOSYS => {
                 let msg = "FBInk was built without image support".into();
@@ -137,69 +121,42 @@ impl FbInk {
         }
     }
 
-    /// Dump the contents of the framebuffer. Clones the data from FBInk to avoid safety issues
-    pub fn dump(&mut self, encoding: Option<ImageOutputFormat>) -> Result<FbInkDump, FbInkError> {
-        if self.raw_dump.is_none() {
-            let raw_dump = MaybeUninit::<raw::FBInkDump>::zeroed();
-            self.raw_dump = Some(unsafe { raw_dump.assume_init() });
-        }
-        let dump = &mut self.raw_dump.unwrap();
+    /// Dump the contents of a specific region of the framebuffer
+    pub fn region_dump(
+        &mut self,
+        x: i16,
+        y: i16,
+        width: u16,
+        height: u16,
+    ) -> Result<FbInkDump, FbInkError> {
+        let mut dump = MaybeUninit::<raw::FBInkDump>::zeroed();
         // returns negative error codes
-        let rv = unsafe { raw::fbink_dump(self.fbfd, dump) };
-        dbg!(&dump);
+        let rv = unsafe {
+            raw::fbink_region_dump(
+                self.fbfd,
+                x,
+                y,
+                width,
+                height,
+                &self.config.into(),
+                dump.as_mut_ptr(),
+            )
+        };
         match -rv {
-            libc::EXIT_SUCCESS => (),
-            libc::EXIT_FAILURE => return Err(FbInkError::ExitFailure("dump".into())),
+            libc::EXIT_SUCCESS => Ok(FbInkDump::new(unsafe { dump.assume_init() })),
+            libc::EXIT_FAILURE => Err(FbInkError::ExitFailure("dump".into())),
+            libc::EINVAL => Err(FbInkError::InvalidArgument("empty region".into())),
             libc::ENOSYS => {
                 let msg = "FBInk was built without image support".into();
-                return Err(FbInkError::NotSupported(msg));
+                Err(FbInkError::NotSupported(msg))
             }
-            x => return Err(FbInkError::Other(x)),
+            x => Err(FbInkError::Other(x)),
         }
-        let raw_buf: &[u8] = unsafe { slice::from_raw_parts_mut(dump.data, dump.size) };
-
-        let buf = if let Some(ref encoding) = encoding {
-            let color_type = match dump.bpp {
-                32 => ColorType::Rgba8,
-                24 => ColorType::Rgb8,
-                // not sure if these are correct
-                16 => ColorType::L16,
-                8 => ColorType::L8,
-                _ => {
-                    let msg = format!("Can't encode a dump with {} bpp", dump.bpp);
-                    return Err(FbInkError::NotSupported(msg));
-                }
-            };
-            let mut writer = Cursor::new(Vec::new());
-            image::write_buffer_with_format(
-                &mut writer,
-                raw_buf,
-                dump.area.width.into(),
-                dump.area.height.into(),
-                color_type,
-                encoding.clone(),
-            )
-            .unwrap();
-            writer.into_inner()
-        } else {
-            raw_buf.to_vec()
-        };
-
-        Ok(FbInkDump {
-            data: buf,
-            stride: dump.stride,
-            area: dump.area,
-            clip: dump.clip,
-            rota: dump.rota,
-            bpp: dump.bpp,
-            is_full: dump.is_full,
-            encoding,
-        })
     }
 
-    /// Restore the contents of a raw dump back to the framebuffer.
-    pub fn restore(&mut self, raw_dump: &raw::FBInkDump) -> Result<(), FbInkError> {
-        let rv = unsafe { raw::fbink_restore(self.fbfd, &self.config.into(), raw_dump) };
+    /// Restore the contents of a dump back to the framebuffer
+    pub fn restore(&self, dump: &FbInkDump) -> Result<(), FbInkError> {
+        let rv = unsafe { raw::fbink_restore(self.fbfd, &self.config.into(), dump.raw_dump()) };
         match -rv {
             libc::EXIT_SUCCESS => Ok(()),
             libc::EXIT_FAILURE => Err(FbInkError::ExitFailure("restore".into())),
@@ -218,7 +175,10 @@ impl FbInk {
         if state.is_sunxi {
             Ok(dump_sunxi(Some(encoding), state.current_rota)?)
         } else {
-            Ok(self.dump(Some(encoding))?.data)
+            // On some devices the dump contains junk pixels outside the visible framebuffer
+            let (width, height) = (state.view_width as u16, state.view_height as u16);
+            let dump = self.region_dump(0, 0, width, height)?;
+            Ok(dump.encode(encoding)?)
         }
     }
 }
@@ -231,90 +191,5 @@ flags! {
         RotationChanged = raw::OK_ROTA_CHANGE,
         LayoutChanged = raw::OK_LAYOUT_CHANGE,
         GrayscaleChanged = raw::OK_GRAYSCALE_CHANGE,
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct FbInkDump {
-    pub data: Vec<u8>,
-    pub stride: usize,
-    pub area: raw::FBInkRect,
-    pub clip: raw::FBInkRect,
-    pub rota: u8,
-    pub bpp: u8,
-    pub is_full: bool,
-    pub encoding: Option<ImageOutputFormat>,
-}
-
-// Attempted to convert a FbInkDump back to a raw::FBInkDump, but it causes a segfault when
-// passed to fbink_restore
-
-// impl TryFrom<FbInkDump> for raw::FBInkDump {
-//     type Error = FbInkError;
-//
-//     fn try_from(mut dump: FbInkDump) -> Result<Self, Self::Error> {
-//         if dump.encoding.is_some() {
-//             return Err(FbInkError::NotSupported(
-//                 "Can't convert an encoded dump".into(),
-//             ));
-//         }
-//         Ok(raw::FBInkDump {
-//             data: dump.data.as_mut_ptr(),
-//             size: dump.data.len(),
-//             stride: dump.stride,
-//             area: dump.area,
-//             clip: dump.clip,
-//             rota: dump.rota,
-//             bpp: dump.bpp,
-//             is_full: dump.is_full,
-//         })
-//     }
-// }
-
-// Adapted from FBInk/utils/sunxigrab.sh
-fn dump_sunxi(
-    encoding: Option<ImageOutputFormat>,
-    current_rota: u8,
-) -> Result<Vec<u8>, FbInkError> {
-    // Path is hard-coded by the kernel
-    let mount_path = PathBuf::from("/mnt/flash");
-    let bmp_path = mount_path.join("workingbuffer.bmp");
-    fs::create_dir_all(&mount_path).unwrap();
-
-    if !MountIter::new()?.any(|m| m.unwrap().dest == mount_path) {
-        Command::new("mount")
-            .arg("-t")
-            .arg("tmpfs")
-            .arg("tmpfs")
-            .arg(mount_path)
-            .arg("-o")
-            .arg("noatime,size=3M")
-            .output()?;
-    }
-
-    // Trigger a dump of the framebuffer by reading from the sysfs file
-    let sysfs_path = "/sys/devices/virtual/disp/disp/waveform/get_working_buffer";
-    let rv: i32 = fs::read_to_string(sysfs_path)?.trim().parse().unwrap();
-
-    if rv == 0 {
-        println!("Working buffer dumped to {}", bmp_path.display());
-    } else {
-        println!("Failed to dump the working buffer!");
-    }
-    let mut decoded = image::io::Reader::open(bmp_path)?.decode()?;
-    imageops::flip_vertical_in_place(&mut decoded);
-    let decoded = match current_rota {
-        0 => decoded.rotate270(),
-        2 => decoded.rotate90(),
-        3 => decoded.rotate180(),
-        _ => decoded,
-    };
-
-    if let Some(encoding) = encoding {
-        let mut encoded = Cursor::new(Vec::new());
-        decoded.write_to(&mut encoded, encoding)?;
-        Ok(encoded.into_inner())
-    } else {
-        Ok(decoded.into_bytes())
     }
 }
